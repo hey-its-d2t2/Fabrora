cb2de9d5ac531c818ebf19ba955aae02b471aceb
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-25 11:33:52 +0530

    Initial Commit.

---
+++ src/main/java/com/Fabrora/FabroraApplication.java
@@ -0,0 +1,13 @@
+package com.Fabrora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabroraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabroraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Fabrora/FabroraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fabrora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabroraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
